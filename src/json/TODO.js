84386ceb3084c72d591781066f5868b21e3a2348
thelibertti <139710771+thelibertti@users.noreply.github.com>
2024-07-12 21:56:40 -0600

    First commit :)

---
+++ src/json/TODO.js
@@ -0,0 +1,15 @@
+const tasks =[ 
+  {
+    "Name": "create basic layout",
+    "Description": "Create a basic layout of how the webpage will look in the future",
+    "Status": "Pending",
+  },
+
+  {
+    "Name": "Add the header",
+    "Description": "Add the header of the webpage plus the basic options such as 'go to github' or 'donations'",
+    "Status": "Pending"
+  },
+];
+
+export default tasks;
